59ac28ade6c9fd6738599f800cb8561d23ed3c84
dowglasOficial <bittencourtdouglas41@gmail.com>
2026-06-10 17:13:15 -0300

    create folder bean in folder config, create method securityFilterChain

---
+++ ClientService/src/main/java/com/app/config/bean/ClientBean.java
@@ -0,0 +1,26 @@
+package com.app.config.bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ClientBean {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
